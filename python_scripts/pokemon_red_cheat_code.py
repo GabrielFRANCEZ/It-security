1aa79c090e9ecc47bd0cf70e94e5da55cdbe9849
Your Name <you@example.com>
2023-07-27 00:02:05 +0200

    Creating cheats to modify money and badges in Pokemon Red

---
+++ python_scripts/pokemon_red_cheat_code.py
@@ -0,0 +1,79 @@
+import sys
+import argparse
+import math 
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", dest="savepath", help="Specify path to the save file", required=True)
+    parser.add_argument("-m", "--money", dest="money", help="Specify the amount of money", required=False)
+    parser.add_argument("-b", "--badge", dest="badge", help="Specify the number of badge", required=False)
+
+    arguments = parser.parse_args()
+    return arguments
+
+def fix_checksum(ram):
+    checksum = 0xff
+    for c in ram [0x2598:0x3523]:
+        checksum -= c
+    
+    ram[0x3523] = checksum&0xff
+    #print("[+] Checksum {} written".format(checksum&0xff))
+    
+def decimal_to_bcd(decimal_value):
+    bcd_value = bytearray()
+    # Convert the decimal value to BCD format
+    while decimal_value > 0:
+        digit = decimal_value % 10
+        decimal_value //= 10
+        bcd_byte = (digit << 4) & 0xF0
+        digit = decimal_value % 10
+        bcd_byte |= digit & 0x0F
+        bcd_value.append(bcd_byte)
+
+    bcd_value.reverse()
+    return bcd_value
+
+def get_hex(number):
+    unit = number % 10
+    dec = number // 10
+
+    return 16 * dec + unit 
+
+def change_money (ram, money):
+    amount = int(money)
+    if amount > 999999:
+         print("[-] ERROR MAXIMUM IS 999,999 and you enter : {}".format( amount))
+         return 1
+    else :
+        value = amount % 100
+        addr_memoire = 0x25F5
+        ram[addr_memoire] = get_hex(value)
+        amount = amount // 100
+        value = amount % 100
+        addr_memoire = 0x25F4
+        ram[addr_memoire] = get_hex(value)
+        amount = amount // 100
+        value = amount % 100
+        addr_memoire = 0x25F3
+        ram[addr_memoire] = get_hex(value)
+        amount = amount // 100
+        print("[+] Now, you have {} P$".format(money))
+    
+def change_badge(ram, badge) : 
+    value = pow(2,int(badges)) - 1
+    res = int(hex(value),16)
+    ram[0x2602] = res
+    print("[+] Now, you have {} badges".format(badge))
+
+arguments = get_arguments()
+with open(arguments.savepath,'rb+') as hex_file :
+    ram = bytearray(hex_file.read())
+    #fix_checksum(hex_file)
+    if arguments.money != None : 
+        change_money(ram,arguments.money)
+    if arguments.badge != None : 
+        change_badge(ram,arguments.badge)
+    fix_checksum(ram)
+    hex_file.seek(0,0)
+    hex_file.write(ram)
+    
